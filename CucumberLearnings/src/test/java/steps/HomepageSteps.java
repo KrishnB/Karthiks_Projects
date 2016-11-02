f5c0ed4ef5b8cd355f05bf61b9fea4575269ded6
karthik45 <karthikpenujuru@gmail.com>
2016-11-02 15:36:28 +0530

    added cucumber project

---
+++ CucumberLearnings/src/test/java/steps/HomepageSteps.java
@@ -0,0 +1,213 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by karthik on 27/10/16.
+ */
+public class HomepageSteps {
+
+    @Given("^In a browser I enter \"([^\"]*)\"$")
+    public void inABrowserIEnter(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I should be navigated to homepage$")
+    public void iShouldBeNavigatedToHomepage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on login button$")
+    public void iClickOnLoginButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter password as \"([^\"]*)\"$")
+    public void iEnterPasswordAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click on submit button$")
+    public void iClickOnSubmitButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should be able view my account details$")
+    public void iShouldBeAbleViewMyAccountDetails() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should not be able view my account details$")
+    public void iShouldNotBeAbleViewMyAccountDetails() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter Emailaddress as \"([^\"]*)\"$")
+    public void iEnterEmailaddressAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on Signup button$")
+    public void iClickOnSignupButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter Email as \"([^\"]*)\"$")
+    public void iEnterEmailAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter Confirm Email as \"([^\"]*)\"$")
+    public void iEnterConfirmEmailAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter confirm password as \"([^\"]*)\"$")
+    public void iEnterConfirmPasswordAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    @Then("^I should not be able to create account$")
+    public void iShouldNotBeAbleToCreateAccount() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click on facebookIcon button$")
+    public void iClickOnFacebookIconButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I should be navigated to facebook page$")
+    public void iShouldBeNavigatedToFacebookPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click on GooglePlusIcon button$")
+    public void iClickOnGooglePlusIconButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I should be navigated to googlePlus page$")
+    public void iShouldBeNavigatedToGooglePlusPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter the From location as \"([^\"]*)\"$")
+    public void iEnterTheFromLocationAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter the To location as \"([^\"]*)\"$")
+    public void iEnterTheToLocationAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter Pickup as \"([^\"]*)\"$")
+    public void iEnterPickupAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter time as \"([^\"]*)\"$")
+    public void iEnterTimeAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click on Single Trip$")
+    public void iClickOnSingleTrip() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+    
+
+    @And("^I enter How many passengers as \"([^\"]*)\"$")
+    public void iEnterHowManyPassengersAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter Any Additional Luggage as \"([^\"]*)\"$")
+    public void iEnterAnyAdditionalLuggageAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click on getquote button$")
+    public void iClickOnGetquoteButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I should not be able to book a cab$")
+    public void iShouldNotBeAbleToBookACab() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click on Round Trip$")
+    public void iClickOnRoundTrip() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I should be able to book a cab$")
+    public void iShouldBeAbleToBookACab() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I select a Title as \"([^\"]*)\"$")
+    public void iSelectATitleAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter as FirstName as \"([^\"]*)\"$")
+    public void iEnterAsFirstNameAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter as LastName as \"([^\"]*)\"$")
+    public void iEnterAsLastNameAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter Contact mobile number as \"([^\"]*)\"$")
+    public void iEnterContactMobileNumberAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I enter Confirm mobile number as \"([^\"]*)\"$")
+    public void iEnterConfirmMobileNumberAs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
---
+++ CucumberLearnings/src/test/resources/features/Homepage.feature
@@ -0,0 +1,123 @@
+Feature: Homepage
+  Verify minicabit homepage
+
+
+Scenario: Login
+  Given In a browser I enter "https://www.minicabit.com/"
+  And I should be navigated to homepage
+  When I click on login button
+  And I enter password as "mincabit"
+  And I click on submit button
+  Then I should be able view my account details
+
+
+Scenario: Invalid Login
+  Given In a browser I enter "https://www.minicabit.com/"
+  And I should be navigated to homepage
+  When I click on login button
+  And I enter password as "snand"
+  And I click on submit button
+  Then I should not be able view my account details
+
+Scenario: Invalid Username
+  Given In a browser I enter "https://www.minicabit.com/"
+  And I should be navigated to homepage
+  When I click on login button
+  And I enter Emailaddress as "kakka@gmmm.com"
+  And  I click on login button
+  Then  I should not be able view my account details
+
+
+Scenario: Signup
+  Given In a browser I enter "https://www.minicabit.com/"
+  And I should be navigated to homepage
+  When I click on Signup button
+  And I enter Email as "kakka@gmmm.com"
+  And I enter Confirm Email as ""
+  And I enter password as "Minicabit@45"
+  And I enter confirm password as "Minicabit@45"
+  And I select a Title as "Mr"
+  And I enter as FirstName as "kakaka"
+  And I enter as LastName as "emilia"
+  And I enter Contact mobile number as "12345678901"
+  And I enter Confirm mobile number as "12345678901"
+  Then I should not be able to create account
+
+Scenario:PasswordCheck
+  Given In a browser I enter "https://www.minicabit.com/"
+  And I should be navigated to homepage
+  When I click on Signup button
+  And I enter Email as "kakka@gmmm.com"
+  And I enter Confirm Email as "kakka@gmmm.com"
+  And I enter password as "Minic"
+  And I enter confirm password as "Minic"
+  And I select a Title as "Mr"
+  And I enter as FirstName as "kakaka"
+  And I enter as LastName as "emilia"
+  And I enter Contact mobile number as "12345678901"
+  And I enter Confirm mobile number as "12345678901"
+  Then I should not be able to create account
+
+Scenario:Social Media Page
+  Given In a browser I enter "https://www.minicabit.com/"
+  And I should be navigated to homepage
+  And I click on facebookIcon button
+  And I should be navigated to facebook page
+
+Scenario:Social Media Page
+  Given In a browser I enter "https://www.minicabit.com/"
+  And I should be navigated to homepage
+  And I click on GooglePlusIcon button
+  And I should be navigated to googlePlus page
+
+Scenario:Invalid Booking
+  Given In a browser I enter "https://www.minicabit.com/"
+  And I should be navigated to homepage
+  And I enter the From location as "Manchester Airport"
+  And I enter the To location as "Manchester Airport"
+  And I enter Pickup as "28-10-2016"
+  And I enter time as "10:45"
+  And I click on Single Trip
+  And I enter How many passengers as "1Passenger"
+  And I enter Any Additional Luggage as "3 suitcases"
+  And I click on getquote button
+  And I should not be able to book a cab
+
+Scenario:Invalid Booking
+    Given In a browser I enter "https://www.minicabit.com/"
+    And I should be navigated to homepage
+    And I enter the From location as "Manchester Airport"
+    And I enter the To location as ""
+    And I enter Pickup as "28-10-2016"
+    And I enter time as "10:45"
+    And I click on Single Trip
+    And I enter How many passengers as "1Passenger"
+    And I enter Any Additional Luggage as "3 suitcases"
+    And I click on getquote button
+    And I should not be able to book a cab
+
+  Scenario:Invalid Booking
+    Given In a browser I enter "https://www.minicabit.com/"
+    And I should be navigated to homepage
+    And I enter the From location as "Manchester Airport"
+    And I enter the To location as ""
+    And I enter Pickup as "28-10-2016"
+    And I enter time as "10:45"
+    And I click on Round Trip
+    And I enter How many passengers as "1Passenger"
+    And I enter Any Additional Luggage as "3 suitcases"
+    And I click on getquote button
+    And I should not be able to book a cab
+
+  Scenario:valid Booking
+    Given In a browser I enter "https://www.minicabit.com/"
+    And I should be navigated to homepage
+    And I enter the From location as "Manchester Airport"
+    And I enter the To location as "Manchester Airport"
+    And I enter Pickup as "28-10-2016"
+    And I enter time as "10:45"
+    And I click on Single Trip
+    And I enter How many passengers as "16Passenger"
+    And I enter Any Additional Luggage as "16 suitcases"
+    And I click on getquote button
+    And I should be able to book a cab
